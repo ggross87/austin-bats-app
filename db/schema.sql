ecc58b780d0cd5104e83ad1641352102d7e0bbb6
Germanual Gross <germanualgross@Germanuals-MacBook-Air.local>
2017-08-11 14:07:32 -0500

    server and login added on

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,12 @@ CREATE DATABASE bat_app;
 USE bat_app;
 
 CREATE TABLE Users
- (`id` INTEGER NOT NULL auto_increment ,
-  `email` VARCHAR(255) NOT NULL UNIQUE,
-  `password` VARCHAR(255) NOT NULL,
-  `createdAt` DATETIME NOT NULL,
-  `updatedAt` DATETIME NOT NULL,
-  UNIQUE `Users_email_unique` (`email`),
-  PRIMARY KEY (`id`)
+ (
+  id INTEGER NOT NULL auto_increment ,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NOT NULL,
+  UNIQUE Users_email_unique (email),
+  PRIMARY KEY (id)
 );
