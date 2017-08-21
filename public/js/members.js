65b9de317f53cf64600a06b3a2bd9597fc781b12
Josh Hulsey <jhul0388@gmail.com>
2017-08-21 09:25:37 -0500

    joshpush

--- public/js/members.js
+++ public/js/members.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
   });
 });
 
-var temp2;
-var fah;
-var cloudy;
-var windSpeed;
-var sunsetTime;
-var visibility;
+// var temp2;
+// var fah;
+// var cloudy;
+// var windSpeed;
+// var sunsetTime;
+// var visibility;
 var key = 'cdaff73a26ba20e03fbd4c2f9f18e75e';
 var url = 'https://api.darksky.net/forecast/' + key + '/';
 var cb = '/?callback=?';
@@ -24,23 +24,22 @@ function getLocation() {
 function getWeather(lat, long) {
   $.getJSON(url + lat + ',' + long + cb)
   .then(function(data) {
-    // cloudy = data.daily.data[0].cloudCover.toFixed();
-    visibility = data.daily.data[0].visibility.toFixed();
-    windSpeed = data.currently.windSpeed.toFixed();
-    fah = data.currently.temperature.toFixed();
-    sunsetTime = data.daily.data[0].sunsetTime.toFixed();
+
+    console.log(data);
     $("#todayInfo").html("Current Temperature: " + data.currently.temperature.toFixed());
     $("#todayInfo2").html("The Sun sets at: " + data.daily.data[0].sunsetTime.toFixed());
     $("#todayInfo3").html("Wind Speeds expected to reach: " + data.currently.windSpeed.toFixed());
     $("#todayInfo4").html("Current visibility level is: " + data.daily.data[0].visibility.toFixed());
-    $("#tomorrowInfo").html("Current Temperature: " + data.currently.temperature.toFixed());
-    $("#tomorrowInfo2").html("The Sun sets at: " + data.daily.data[0].sunsetTime.toFixed());
-    $("#tomorrowInfo3").html("Wind Speeds expected to reach: " + data.currently.windSpeed.toFixed());
-    $("#tomorrowInfo4").html("Current visibility level is: " + data.daily.data[0].visibility.toFixed());
-    $("#twoDayInfo").html("Current Temperature: " + data.currently.temperature.toFixed());
-    $("#twoDayInfo2").html("The Sun sets at: " + data.daily.data[0].sunsetTime.toFixed());
-    $("#twoDayInfo3").html("Wind Speeds expected to reach: " + data.currently.windSpeed.toFixed());
-    $("#twoDayInfo4").html("Current visibility level is: " + data.daily.data[0].visibility.toFixed());
+
+    $("#tomorrowInfo").html("Current Temperature: " + data.daily.data[1].temperatureMax.toFixed());
+    $("#tomorrowInfo2").html("The Sun sets at: " + data.daily.data[1].sunsetTime.toFixed());
+    $("#tomorrowInfo3").html("Wind Speeds expected to reach: " + data.daily.data[1].windSpeed.toFixed());
+    $("#tomorrowInfo4").html("Current visibility level is: " + data.daily.data[1].visibility.toFixed());
+
+    $("#twoDayInfo").html("Current Temperature: " + data.daily.data[2].temperatureMax.toFixed());
+    $("#twoDayInfo2").html("The Sun sets at: " + data.daily.data[2].sunsetTime.toFixed());
+    $("#twoDayInfo3").html("Wind Speeds expected to reach: " + data.daily.data[2].windSpeed.toFixed());
+    $("#twoDayInfo4").html("Current visibility level is: " + data.daily.data[2].visibility.toFixed());
   });
 }
 getLocation();
